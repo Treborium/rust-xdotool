047a9f17b862e3011eaec1f86b196d3b38881fe2
Robert Fuchs <robertfuchs97@gmail.com>
2020-01-26 20:37:43 +0100

    Create generic run command

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
+use std::process::Command;
+use std::process::ExitStatus;
+
 pub mod keyboard;
 pub mod command;
+
+
+pub fn run(command: command::Command, args: &str) -> ExitStatus {
+    Command::new("sh")
+        .arg("-c")
+        .arg(format!("xdotool {} {}", command, args))
+        .status()
+        .expect(&format!("Failed to execute 'xdotool key {}", args))
+}
+
 
 #[cfg(test)]
 mod tests {

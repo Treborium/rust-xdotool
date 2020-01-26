c8e77ff47478e551e05eea012a2fb40fc661a19d
Robert Fuchs <robertfuchs97@gmail.com>
2020-01-26 21:01:08 +0100

    Allow to create an empty OptionVec

--- src/command.rs
+++ src/command.rs
@@ -3,16 +3,25 @@ use std::fmt;
 #[derive(Debug)]
 pub struct OptionVec<T: fmt::Display>(pub Vec<T>);
 
+impl<T: fmt::Display> OptionVec<T> {
+    pub fn new() -> OptionVec<T> {
+        OptionVec(Vec::new())
+    }
+}
+
 impl<T: fmt::Display> fmt::Display for OptionVec<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut space_separated = String::new();
         
-        for thing in &self.0[0..self.0.len() - 1] {
-            space_separated.push_str(&thing.to_string());
-            space_separated.push_str(" ");
+        if !self.0.is_empty() {
+            for thing in &self.0[0..self.0.len() - 1] {
+                space_separated.push_str(&thing.to_string());
+                space_separated.push_str(" ");
+            }
+            
+            space_separated.push_str(&self.0[self.0.len() - 1].to_string());
         }
         
-        space_separated.push_str(&self.0[self.0.len() - 1].to_string());
         write!(f, "{}", space_separated)
     }
 }

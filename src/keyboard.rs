73d35362b713b8d180b75528a2ca09992e78d2ec
Robert Fuchs <robertfuchs97@gmail.com>
2020-01-27 11:23:57 +0100

    Add key down and key up functions

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -8,3 +8,13 @@ pub fn send_key(keys: &str, options: OptionVec<KeyboardOption>) -> Output {
     let c = Command::Keyboard(sub_commands::Keyboard::Key(options));
     run(c, keys)
 }
+
+pub fn send_key_down(keys: &str, options: OptionVec<KeyboardOption>) -> Output {
+    let c = Command::Keyboard(sub_commands::Keyboard::KeyDown(options));
+    run(c, keys)
+}
+
+pub fn send_key_up(keys: &str, options: OptionVec<KeyboardOption>) -> Output {
+    let c = Command::Keyboard(sub_commands::Keyboard::KeyUp(options));
+    run(c, keys)
+}
